4ccae640d35b7a609095214c794c9a8115d96c05
Vitor Rodrigues <vitorborbarodrigues@gmail.com>
2024-02-06 22:29:50 -0300

    first commit

---
+++ functions/src/index.ts
@@ -0,0 +1,55 @@
+import * as functions from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
+
+// It gets the credentials from our firebase login step on CLI.
+// In production, it's gonna be running on out cloud project,
+// so it has the credentials there
+initializeApp();
+
+const firestore = new Firestore();
+const storage = new Storage();
+
+const rawVideoBucketName = "vbrodrigues-yt-raw-videos";
+
+export const createUser = functions.auth.user().onCreate((user) => {
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+  };
+
+  firestore.collection("users").doc(user.uid).set(userInfo);
+
+  logger.info(`User created: ${JSON.stringify(userInfo)}`);
+  return;
+});
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+  // Check if the user sent automatically by Cloud Functions is authenticated
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The functions must be called while authenticated."
+    );
+  }
+
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  // Generate a unique filename
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // Get a v4 signed URL for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+
+  return {url, fileName};
+});
